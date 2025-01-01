1aafbd85a5185a52c3bb92edad62d3d33ea62d20
Tomasz Roszczynialski <roszcz@gmail.com>
2025-01-01 23:04:14 +0100

    Compare metrics on a per-task basis

--- dashboards/piano_metrics_review.py
+++ dashboards/piano_metrics_review.py
@@ -3,6 +3,7 @@ import fortepyan as ff
 import streamlit as st
 import streamlit_pianoroll
 
+from piano_dataset import PianoTasks
 from dashboards.utils import dataset_configuration
 from piano_metrics.piano_metric import MetricsRunner
 
@@ -35,31 +36,55 @@ def main():
             min_value=1,
             max_value=piece.size,
         )
-    target_piece = piece[piece_start:piece_finish]
+    piece_part = piece[piece_start:piece_finish]
+
+    available_tasks = PianoTasks.list_tasks()
+    task_name = st.selectbox(
+        label="Select PIANO task",
+        options=available_tasks,
+    )
+    piano_task = PianoTasks.get_task(task_name=task_name)
+    piece_split = piano_task.prompt_target_split(piece_part.df)
+
+    source_piece = ff.MidiPiece(
+        df=piece_split.source_df.copy(),
+    )
+    target_piece = ff.MidiPiece(
+        df=piece_split.target_df.copy(),
+    )
 
     # This makes a copy of the DataFrame inside the piece
-    generated_piece = target_piece[:]
+    generated_df = piece_split.target_df.copy()
 
     # Apply some noise to the fake generated piece
     pitch_noise = np.random.randint(
         low=-1,
         high=1,
-        size=generated_piece.size,
+        size=generated_df.shape[0],
     )
-    generated_piece.df.pitch += pitch_noise
+    generated_df.pitch += pitch_noise
+    generated_piece = ff.MidiPiece(
+        df=generated_df,
+    )
 
     cols = st.columns(2)
     with cols[0]:
-        streamlit_pianoroll.from_fortepyan(target_piece)
+        streamlit_pianoroll.from_fortepyan(
+            piece=source_piece,
+            secondary_piece=target_piece,
+        )
 
     with cols[1]:
-        streamlit_pianoroll.from_fortepyan(generated_piece)
+        streamlit_pianoroll.from_fortepyan(
+            piece=source_piece,
+            secondary_piece=generated_piece,
+        )
 
     metrics_manager = MetricsRunner.load_default()
 
     metric_results = metrics_manager.calculate_all(
-        target_df=target_piece.df,
-        generated_df=generated_piece.df,
+        target_df=piece_split.target_df,
+        generated_df=generated_df,
     )
     for metric_class, metric_result in metric_results.items():
         n_metrics = len(metric_result.metrics)
