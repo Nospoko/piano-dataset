6a4d953c69d7c781ed1d8e00e2920dedc91630cb
Tomek Roszczynialski <roszcz@users.noreply.github.com>
2025-02-03 13:22:00 +0100

    Drop old implementation and remove the "parametric" identifier from PianoTasks (#2)
    * Drop old implementation and remove the "parametric" identifier from PIANO tasks
    
    * Fix the metrics dashboard

--- src/piano_dataset/__init__.py
+++
@@ -1,6 +0,0 @@
-from piano_dataset.piano_task import PianoTask
-from piano_dataset.piano_tasks import PianoTaskManager
-
-PianoTasks = PianoTaskManager()
-for subclass in PianoTask.__subclasses__():
-    PianoTasks.register_task(subclass)
--- src/piano_dataset/main.py
+++
@@ -1,5 +0,0 @@
-def foo():
-    print("bar")
-    print("bar")
-    print("bar")
-    print("bar")
